772012eb9314fa2504c64c74399b4d1f35d5b316
isobelleanna <67749649+isobelleanna@users.noreply.github.com>
2022-08-29 15:07:05 +0100

    create basic calculator

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,2 +1,58 @@
-package org.example;public class Calculator {
+package org.example;
+
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Calculator {
+
+    int num2;
+
+    int o = 0;
+
+    String operator;
+
+    UserInput userInput = new UserInput();
+
+    public void runCalculator(){
+        boolean isActive = true;
+        userInput.printMessage("Enter a number");
+        o = userInput.getNumber();
+        while (isActive) {
+            userInput.printMessage("Enter a number");
+            num2 = userInput.getNumber();
+            userInput.printMessage("Enter operator(+*/-=)");
+            operator = userInput.getOperator();
+            calculate(o, num2);
+            userInput.printMessage("continue [y/n]");
+            String input = userInput.getString();
+            if (Objects.equals(input, "n")){
+                isActive = false;
+            }
+        }
+
+    }
+
+    public void calculate(int num1, int num2){
+
+        switch (operator){
+            case "+":
+                o = num1 + num2;
+                break;
+            case "-":
+                o = num1 - num2;
+                break;
+            case "*":
+                o = num1 * num2;
+                break;
+            case "/":
+                o = num1 / num2;
+                break;
+            default:
+                userInput.printMessage("Not an operator");
+        }
+        userInput.printMessage("answer is " + o);
+    }
+
+
+
 }
